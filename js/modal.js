f4cf6478aedce815b795ddfb544372ca785b806e
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-26 10:25:14 +0200

    dde

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,18 @@
 const input = [
-    ["a", "b", "c"],
+    ["a", "b", "c", "f"],
     ["c", "d", "f"],
     ["d", "f", "g"],
 ];
 
-
-const output = input.flat()
+const output = input.flat().reduce((accumulator, currentValue) => {
+    console.log(accumulator, currentValue)
+    if (accumulator[currentValue]) {
+        //console.log(accumulator[currentValue])
+        accumulator[currentValue] += 1;
+    } else {
+        accumulator[currentValue] = 1;
+    }
+    return accumulator;
+},{});
 
 console.log(output)

e85216f552515e9ceed2f198cf408a20412c3e21
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-28 23:54:45 +0200

    newe

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,21 @@
-const everyList = document.querySelectorAll('ul')
+const divBox = document.getElementById("list-app")
+const text = document.createElement("p")
+const ulList = document.createElement("ul")
+const input = document.createElement("input")
+const button = document.createElement("button")
 
+text.innerHTML = "Click on the item to change the text, or click on the button to create a new item."
+button.innerHTML = "Create New"
 
-everyList.forEach((element)=> {
-    element.firstElementChild.innerHTML = "First"
-    element.lastElementChild.innerHTML = "Last"
-})
+divBox.appendChild(text)
+divBox.appendChild(ulList)
+divBox.appendChild(input)
+divBox.appendChild(button)
+
+const addElement = () => {
+    const liList = document.createElement("li")
+    ulList.appendChild(liList)
+    liList.innerHTML = input.value
+}
+
+button.addEventListener("click", addElement)

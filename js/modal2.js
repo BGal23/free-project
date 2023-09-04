6dd69262dc3b7a2b7c6b6290cbb23bec6e032e90
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-04 23:10:45 +0200

    test

--- js/modal2.js
+++ js/modal2.js
@@ -1,3 +1,28 @@
-let potions = [1,2,3];
-let a = 2;
-console.log(potions.includes(a));
+const button = document.querySelector(".buttons-box");
+const text = document.querySelector(".text")
+
+const yourText = (event) => {
+    if (event.target.nodeName !== "BUTTON") {
+        return text.textContent = "";
+    }
+    switch (event.target.innerText) {
+        case "First":
+            color = "blue";
+            font = "15px";
+            break;
+        case "Second":
+            color = "red";
+            font = "25px";
+            break;
+        case "Third":
+            color = "green";
+            font = "20px";
+            break;
+    }
+
+    text.textContent = " " + event.target.innerText
+    text.style.color = color;
+    text.style.fontSize = font;
+}
+
+button.addEventListener("click", yourText)
